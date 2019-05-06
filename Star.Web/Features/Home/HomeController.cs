37d01bf49afe3a04de21dafde3e5009db4c04af0
Hoshi Taufig <hoshi.taufig@creuna.se>
2019-05-06 11:51:04 +0200

    Add directory/namespace structure with seperated features

---
+++ Star.Web/Features/Home/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Star.Web.Features.Home {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
